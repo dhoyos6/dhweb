e9e8e9b1cbf54334a54a5dd5729a58428171ee76
dhoyos6 <43800887+dhoyos6@users.noreply.github.com>
2019-05-21 14:24:48 -0700

    Hooked up the Model to Home and Index View

--- dhweb/Controllers/HomeController.cs
+++ dhweb/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace dhweb.Controllers
                 LastAcessDate = DateTime.Now
             };
 
-            return View();
+            return View(myData);
         }
 
         public ActionResult FAQ()
